5988acdc7e34662e6223f981b3001625259b7d77
Ernur <124793455+A1z3n007@users.noreply.github.com>
2025-11-13 19:11:39 +0500

    Delete flower-shop directory

--- flower-shop/src/assets/icons.js
+++
@@ -1,3 +0,0 @@
-export const LOGO_ICON_URL = "https://cdn.jsdelivr.net/gh/lucide-icons/lucide/icons/flower.svg";
-export const PETAL_ICON_URL = "https://cdn.jsdelivr.net/gh/lucide-icons/lucide/icons/flower.svg";
-/* при желании замени на любые другие SVG-URL */
